fc8237f3819488a829c641e12d5891e18b7d712f
oshuster <godzilasanya@gmail.com>
2024-05-02 21:55:58 +0300

    add async read json

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json' assert { type: "json" };
+import swaggerUi from "swagger-ui-express";
+import fs from "fs/promises";
+import path from "path";
+
+const jsonPath = "./swagger.json";
+const swaggerDocumentPath = path.resolve(jsonPath);
+
+async function readSwaggerDocument() {
+  const data = await fs.readFile(swaggerDocumentPath);
+  const swaggerDocument = JSON.parse(data);
+  return swaggerDocument;
+}
 
 import { authRouter, boardRouter, imagesRouter } from "./routes/api/index.js";
 
@@ -16,11 +26,14 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/auth", authRouter);
 app.use("/api/boards", boardRouter);
 app.use("/api/images", imagesRouter);
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(await readSwaggerDocument())
+);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -31,6 +44,4 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-
 export default app;
-
