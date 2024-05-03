a57c68bb5a74a9855817530a2b464f026bdc4333
Havrylyuk Pavlo <havrylyukpavlo95@gmail.com>
2024-05-03 14:01:37 +0300

    fix and add env info

--- helpers/sendHelp.js
+++ helpers/sendHelp.js
@@ -1,27 +1,23 @@
 import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
-const {
-  SEND_UKR_NET_USER,
-  SEND_UKR_NET_PASS,
-  SEND_UKR_NET_HOST,
-  SEND_UKR_NET_PORT,
-} = process.env;
+const { SEND_MAIL_USER, SEND_MAIL_PASS, SEND_MAIL_HOST, SEND_MAIL_PORT } =
+  process.env;
 
 const config = {
-  host: SEND_UKR_NET_HOST,
-  port: SEND_UKR_NET_PORT,
+  host: SEND_MAIL_HOST,
+  port: SEND_MAIL_PORT,
   secure: true,
   auth: {
-    user: SEND_UKR_NET_USER,
-    pass: SEND_UKR_NET_PASS,
+    user: SEND_MAIL_USER,
+    pass: SEND_MAIL_PASS,
   },
 };
 
 const transporter = nodemailer.createTransport(config);
 
 const sendHelp = data => {
-  const email = { ...data, from: SEND_UKR_NET_USER };
+  const email = { ...data, from: SEND_MAIL_USER };
   return transporter.sendMail(email);
 };
 
