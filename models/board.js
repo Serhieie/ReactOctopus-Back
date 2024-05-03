9bc4a240abb4f2bfe0471cc933b823a7857aaaa1
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-05-03 11:53:53 +0300

    clear commented code

--- models/board.js
+++ models/board.js
@@ -1,10 +1,5 @@
-// const mongoose = require("mongoose");
-// import mongoose from "mongoose";
 import { Schema, model } from "mongoose";
 import { handleSaveError, setUpdateSetting } from "./hooks.js";
-
-// const Schema = mongoose.Schema;
-// const Joi = require("joi");
 
 const boardSchema = new Schema(
   {
@@ -33,17 +28,6 @@ boardSchema.post("save", handleSaveError);
 boardSchema.pre("findOneAndUpdate", setUpdateSetting);
 boardSchema.post("findOneAndUpdate", handleSaveError);
 
-// const createBoardSchema = {};
-
-// const updateBoardSchema = {};
-
-// const schemas = {
-//   createBoardSchema,
-//   updateBoardSchema,
-// };
-
-// const Board = mongoose.model("Board", boardSchema);
-
-// module.exports = { Board, schemas };
 const Board = model("board", boardSchema);
+
 export default Board;
